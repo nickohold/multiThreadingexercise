3c2d9434f3e8362ae7f4e821aa546165d6fb21ec
nickohold <nick.holden@ironsrc.com>
2022-05-24 22:13:55 +0300

    2.1

--- app/src/main/java/com/example/multithreadingexercise/MainActivity.java
+++ app/src/main/java/com/example/multithreadingexercise/MainActivity.java
@@ -4,26 +4,44 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "NICKTag";
     private Handler mMainThreadHandler;
     private Handler mCalcThreadHandler;
     private Handler mSumCalcThreadHandler;
-    private int totalSum = 0;
+    private int mTotalSum = 0;
+    private ArrayList<Integer> mSumsArrayList = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSCrollableViews();
 
         mCalcThreadHandler = getNewThreadHandler("CalcThread");
         mSumCalcThreadHandler = getNewThreadHandler("sumCalcThread");
         mMainThreadHandler = new Handler(Looper.getMainLooper());
+    }
+
+    private void setSCrollableViews() {
+        TextView sumTextView = (TextView) getViewById("sumTextView");
+        TextView totalSumTextView = (TextView) getViewById("totalSumTextView");
+        sumTextView.setMovementMethod(new ScrollingMovementMethod());
+        totalSumTextView.setMovementMethod(new ScrollingMovementMethod());
+        TextView totalSumsArrayTextView = (TextView) getViewById("totalSumsArrayTextView");
+        totalSumsArrayTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public Handler getNewThreadHandler(String threadName) {
@@ -36,56 +54,64 @@ public class MainActivity extends AppCompatActivity {
         mCalcThreadHandler.post(new Runnable() {
             @Override
             public void run() {
-                int sum = getEditTextNum("num1") + getEditTextNum("num2");
+                int sum = getEditTextAsInt("num1") + getEditTextAsInt("num2");
                 finishCalc(sum);
             }
         });
     }
 
     private void finishCalc(int sum) {
-        updateSumUi(Integer.toString(sum));
+        updateSumsVariables(sum);
+        updateUiByView("sumTextView", Integer.toString(sum));
         mSumCalcThreadHandler.post(new Runnable() {
             @Override
             public void run() {
-                totalSum += sum;
-                updateTotalSumUi(Integer.toString(totalSum));
+                updateUiByView("totalSumTextView", Integer.toString(mTotalSum));
             }
         });
 
     }
 
-    private void updateSumUi(String message) {
-        String currentThreadName = Thread.currentThread().getName();
-        String constText = "Updated from thread " + currentThreadName + ": " + message;
-        mMainThreadHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                TextView sumTextView = findViewById(R.id.sumTextView);
-                sumTextView.setText(sumTextView.getText() + "\n" + constText);
-            }
-        });
+    public synchronized void updateSumsVariables(int sum) {
+        try {
+            mSumsArrayList.add(sum);
+            mTotalSum += sum;
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    private void updateTotalSumUi(String message) {
+    private void updateUiByView(String textViewId, String message) {
         String currentThreadName = Thread.currentThread().getName();
-        String constText = "Updated from thread " + currentThreadName + ": " + message;
+        String constText = getString(R.string.upatedFromThread) + currentThreadName + ": " + message;
         mMainThreadHandler.post(new Runnable() {
             @Override
             public void run() {
-                TextView totalSumTextView = findViewById(R.id.totalSumTextView);
+                TextView totalSumTextView = (TextView) getViewById(textViewId);
                 totalSumTextView.setText(totalSumTextView.getText() + "\n" + constText);
             }
         });
     }
 
-    private int getEditTextNum(String id) {
-        int editTextId = getResources().getIdentifier(id, "id", getPackageName());
-        EditText editText = findViewById(editTextId);
+    private int getEditTextAsInt(String id) {
+        EditText editText = (EditText) getViewById(id);
         String temp = editText.getText().toString();
         int num = 0;
         if (!"".equals(temp)){
             num = Integer.parseInt(temp);
         }
         return num;
+    }
+
+    private View getViewById(String id) {
+        int viewId = getResources().getIdentifier(id, "id", getPackageName());
+        return findViewById(viewId);
+    }
+
+    public void updateClick(View v) {
+        TextView totalSumsArrayTextView = findViewById(R.id.totalSumsArrayTextView);
+        totalSumsArrayTextView.setText(TextUtils.join(", ",  mSumsArrayList));
     }
 }
